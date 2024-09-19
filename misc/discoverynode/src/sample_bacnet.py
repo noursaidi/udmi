dfa587253d3ebc82be21039e9d9b92e452e1377b
Noureddine <noureddine@saidi.uk>
2024-09-19 10:11:17 +0000

    copy

---
+++ misc/discoverynode/src/sample_bacnet.py
@@ -0,0 +1,33 @@
+"""Sample for calling BACnet scan directly"""
+import logging
+import multiprocessing
+import sys
+import time
+from unittest import mock
+import udmi.discovery.bacnet
+
+
+def main():
+  stdout = logging.StreamHandler(sys.stdout)
+  stdout.addFilter(lambda log: log.levelno < logging.WARNING)
+  stdout.setLevel(logging.INFO)
+  stderr = logging.StreamHandler(sys.stderr)
+  stderr.setLevel(logging.WARNING)
+  logging.basicConfig(
+      format="%(asctime)s|%(levelname)s|%(module)s:%(funcName)s %(message)s",
+      handlers=[stderr, stdout],
+      level=logging.INFO,
+  )
+  logging.root.setLevel(logging.INFO)
+  state = mock.MagicMock()
+  a = udmi.discovery.bacnet.GlobalBacnetDiscovery(
+      state, print, bacnet_ip=None
+  )
+  a.controller({"discovery": {"families": {"bacnet": {"generation": "123"}}}})
+  while True:
+    time.sleep(1)
+
+
+if __name__ == "__main__":
+  multiprocessing.freeze_support()
+  main()
